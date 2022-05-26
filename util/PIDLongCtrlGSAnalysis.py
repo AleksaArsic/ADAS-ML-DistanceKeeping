f4598b66127acd4a584570822a7c0612340c911e
Aleksa Arsic <arsicaleksa96@gmail.com>
2022-05-26 21:15:57 +0200

    PIDLongCtrlGSAnalysis.py graph drawing.

--- util/PIDLongCtrlGSAnalysis.py
+++ util/PIDLongCtrlGSAnalysis.py
@@ -3,10 +3,11 @@
                                of finding optimal PID Longitudinal Controller parameters (Kp, Ki, Kd)
 '''
 
-from fileinput import filename
 import os
+import sys
 from datetime import datetime
 from pathlib import Path
+import matplotlib.pyplot as plt
 
 import numpy as np
 
@@ -38,10 +39,93 @@ def parseData(data):
     # transpose data
     transposedData = np.transpose(np.array(data))
 
-    return [transposedData[0], transposedData[1]]
+    return [np.float32(transposedData[0]), np.float(transposedData[1])]
+
+def combineThrottleBrake(throttle, brake):
+
+    fun = []
+
+    for i in range(len(throttle)):
+        if (float(throttle[i]) > 0):
+            fun.append(float(throttle[i]))
+        else:
+            fun.append(float(brake[i]))
+
+    return fun
+
+def generateTime(timeStep, timeLen):
+
+    time = []
+
+    time.append(0)
+
+    for i in range(1, timeLen):
+        time.append(time[i - 1] + timeStep)
+
+    return time
+
+def generateIndividualTime(timeStep, throttle, brake):
+
+    throttleLen = len(throttle)
+    brakeLen = len(brake)
+
+    timeLen = 0
+
+    if (throttleLen > brakeLen):
+        timeLen = throttleLen
+    else:
+        timeLen = brakeLen
+
+    time = generateTime(timeStep, timeLen)
+
+    return time
+
+# plot analysis data 
+def plotAnalysis(throttle, brake, throttleBrake, longestTime, time, outputDir):
+
+    #Plottig the throttle and brake functions
+    plt.figure(figsize=(30, 10))
+    plt.plot(longestTime[:len(throttle)], throttle, 'b', label='Throttle function')
+    plt.axhline(y=0, color='black', linestyle='-')
+    plt.title('Throttle analysis in time')
+    plt.xlabel('Time')
+    plt.ylabel('Thorttle')
+    plt.legend()
+    plt.savefig(os.path.join(outputDir, 'throttle.png'))
+
+    plt.clf()
+
+    plt.figure(figsize=(30, 10))
+    plt.plot(longestTime[:len(brake)], brake, 'b', label='Brake function')
+    #plt.axhline(y=0, color='black', linestyle='-')
+    plt.title('Brake analysis in time')
+    plt.xlabel('Time')
+    plt.ylabel('Brake')
+    plt.legend()
+    plt.savefig(os.path.join(outputDir, 'brake.png'))
+
+    plt.clf()
+
+    plt.figure(figsize=(30, 10))
+    plt.plot(time, throttleBrake, 'b')
+    plt.axhline(y=0, color='black', linestyle='-')
+    plt.title('Throttle/Brake analysis in time')
+    plt.xlabel('Time')
+    plt.ylabel('Throttle/Brake')
+    plt.savefig(os.path.join(outputDir, 'throttleBrakeFunction.png'))
 
 if __name__ == "__main__":
     script_start = datetime.now()
+
+    outputFolder = './PIDLongCtrlGSAnalysis_output_' + datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
+
+    # create output folder
+    if not os.path.exists(outputFolder):
+        os.makedirs(outputFolder)
+    else:
+        print("Folder with the name " + str(outputFolder) + " already exists. Possible loss of data.")
+        print("Aborting...")
+        sys.exit()
 
     # get all .csv filenames from directory
     filenames = []
@@ -50,11 +134,14 @@ if __name__ == "__main__":
         if file.endswith(".csv"):
             filenames.append(os.path.join(directory, file))
 
-    print(filenames)
+    # Analyse data for each Grid Search iteration
+    bestThrottle = []
+    bestBrake = []
+    bestThrottleBrakeFun = []
+    for i in range(3):#len(filenames)):
 
-    for i in range(len(filenames)):
         # parse each .csv file
-        data = parseCSVFile('../pid_grid_search/MainSimulation_05_24_2022_20_52_55.csv')
+        data = parseCSVFile(filenames[i])
 
         # sort data into two arrays:
         # [throttle], [brake]
@@ -62,14 +149,25 @@ if __name__ == "__main__":
 
         throttle, brake = transposedData[0], transposedData[1]
 
-        print(throttle)
-        print(brake)
+        throttle = [float(i) for i in throttle]
+        brake = [float(i) for i in brake]
 
-        # TO-DO: process/analyse data
+        # combine throttle and brake in one function
+        throttleBrakeFun = combineThrottleBrake(throttle, brake)
 
-        # save analysis results
+        # calculate how good is throttle, brake, throttleBrakeFun
 
-        print(data)
+        # save best throttle, brake, throttleBrakeFun
+        bestThrottle = throttle
+        bestBrake = brake
+        bestThrottleBrakeFun = throttleBrakeFun
+
+    # save best analysis result
+    longestTime = generateIndividualTime(0.05, throttle, brake)
+    time = generateTime(0.05, len(throttle))
+
+    # plot best analysis result
+    plotAnalysis(throttle, brake, throttleBrakeFun, longestTime, time, outputFolder)
     
     script_end = datetime.now()
     print(script_end - script_start)
